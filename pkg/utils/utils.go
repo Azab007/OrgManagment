625352eb49af90e069b9f6e9bfe99742940cb7d3
Azab007 <mazab322@gmail.com>
2024-02-03 21:28:14 +0200

    update utils

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,27 +1,53 @@
 package utils
 
-// Example using jwt-go package
 import (
-	"OrgManagementApp/pkg/database/mongodb/models"
-	"time"
+	"errors"
+	"fmt"
+	"strings"
 
 	"github.com/dgrijalva/jwt-go"
 )
 
-var jwtSecret = []byte("somesecretjwtcode")
+// ExtractBearerToken extracts the Bearer token from the Authorization header
+func ExtractBearerToken(authorizationHeader string) (string, error) {
+	// Check if the Authorization header has the correct format
+	parts := strings.Split(authorizationHeader, " ")
+	if len(parts) != 2 || parts[0] != "Bearer" {
+		return "", errors.New("invalid Bearer token format")
+	}
 
-func generateAccessToken(user models.User) (string, error) {
-	token := jwt.New(jwt.SigningMethodHS256)
-	claims := token.Claims.(jwt.MapClaims)
-	claims["sub"] = user.Email
-	claims["exp"] = time.Now().Add(time.Hour * 1).Unix() // Access token expiration time
-	return token.SignedString(jwtSecret)
+	return parts[1], nil
 }
 
-func generateRefreshToken(user models.User) (string, error) {
-	token := jwt.New(jwt.SigningMethodHS256)
-	claims := token.Claims.(jwt.MapClaims)
-	claims["sub"] = user.Email
-	claims["exp"] = time.Now().Add(time.Hour * 24 * 7).Unix() // Refresh token expiration time
-	return token.SignedString(jwtSecret)
+var jwtSecret = []byte("somesecretjwtcode")
+
+// VerifyAccessToken verifies the validity of the access token
+// and returns the user ID if the token is valid
+func VerifyAccessToken(accessToken string) (string, error) {
+	// Parse the access token
+	token, err := jwt.Parse(accessToken, func(token *jwt.Token) (interface{}, error) {
+		// Validate the signing method and return the secret key
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("invalid signing method")
+		}
+		return jwtSecret, nil
+	})
+
+	// Check for parsing errors
+	if err != nil || !token.Valid {
+		return "", fmt.Errorf("invalid access token")
+	}
+
+	// Extract user ID from claims
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return "", fmt.Errorf("error extracting claims from access token")
+	}
+
+	email, ok := claims["sub"].(string)
+	if !ok {
+		return "", fmt.Errorf("error extracting user ID from access token")
+	}
+
+	return email, nil
 }
